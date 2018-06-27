71f86eb2f24caa36807adfad92ef005243b2475d
popedub <pablo.volt@gmail.com>
2018-06-27 13:16:10 +0200

    test

--- web/app/themes/villaescusatheme/app/controllers/front-page.php
+++ web/app/themes/villaescusatheme/app/controllers/front-page.php
@@ -6,5 +6,10 @@ use Sober\Controller\Controller;
 
 class FrontPage extends Controller
 {
-
+    public function slider() {
+        $images = get_field('galeria_inicio');
+        if ($images) {
+            return $images;
+        }
+    }
 }
